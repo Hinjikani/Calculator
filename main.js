68e12f589deb57ccc0dc20fe1ac1b438d4465607
Hinjikani <pascalhanif@gmail.com>
2025-02-12 16:25:39 +0700

    Add keyboard support

--- main.js
+++ main.js
@@ -27,6 +27,22 @@ function operate(operator, a, b) {
     }
 }
 
+function equal() {
+    if (temporaryMemory.length != 0){
+        number = parseFloat(temporaryMemory.join(""));
+        memory.push(number);
+    } else if (memory.at(-1) == "+" || memory.at(-1) == "-" || memory.at(-1) == "x" || memory.at(-1) == "/") {
+        memory.pop()
+    }
+    if (memory.length != 0) {
+        screenBottom.textContent = Math.round(calculateMemory(memory) * 1000) / 1000;
+    }
+    temporaryMemory = []
+    memory = []
+    temporaryMemory.push(screenBottom.textContent);
+    screenTop.textContent = screenBottom.textContent;
+}
+
 function calculateMemory(memory) {;
     let result = memory[0];
     for (let i = 1; i < memory.length; i++) {
@@ -46,6 +62,8 @@ function calculateMemory(memory) {;
 function clearScreen() {
     screenTop.textContent = "";
     screenBottom.textContent = "";
+    memory = []
+    temporaryMemory = []
 }
 
 function checkOperator(lastScreenBottom, button) {
@@ -58,6 +76,41 @@ function checkOperator(lastScreenBottom, button) {
     }
 }
 
+function changeLastOperator(keyButton){
+    screenBottom.textContent = screenBottom.textContent.slice(0, -1) + keyButton;
+            memory.pop();
+            memory.push(keyButton);
+}
+
+function keyboardListener() {
+    document.addEventListener('keydown', (event) => {
+        console.log("You pressed: " + event.key);
+        key = event.key
+        if (key === "=" || key === "Enter"){
+            equal()
+        } else if (key === "c"){
+            clearScreen()
+        } else if (checkOperator(screenBottom.textContent.slice(-1), key)) {
+            changeLastOperator(key)
+        } else if (!isNaN(parseFloat(key)) || key == "x" || key == "*"  || key == "/" || key == "+" || key == "-") {
+            screenBottom.textContent += key;
+            if (memory.length == 0 && temporaryMemory.length == 0 && (key === "+" || key === "x" || key === "*" || key === "/")) {
+                changeLastOperator(key)
+            } else if (memory.length == 0 && temporaryMemory.length == 0 && key === "-"){
+                temporaryMemory.push(key)
+            } else if (key === "+" || key === "-" || key === "x" || key === "/"){
+                if (temporaryMemory.length != 0){
+                    number = parseFloat(temporaryMemory.join(""));
+                    memory.push(number);
+                }
+                temporaryMemory = [];
+                memory.push(key)
+            } else {
+                temporaryMemory.push(key)
+            }
+        }
+    });
+}
 
 let screenTop = document.querySelector(".screenTop");
 let screenBottom = document.querySelector(".screenBottom");
@@ -67,34 +120,18 @@ let temporaryMemory = [];
 document.querySelectorAll("button").forEach((button) => {
     button.addEventListener("click", () => {
         if (button.textContent === "=") {
-            if (temporaryMemory.length != 0){
-                number = parseFloat(temporaryMemory.join(""));
-                memory.push(number);
-            } else if (memory.at(-1) == "+" || memory.at(-1) == "-" || memory.at(-1) == "x" || memory.at(-1) == "/") {
-                memory.pop()
-            }
-            if (memory.length != 0) {
-                screenBottom.textContent = Math.round(calculateMemory(memory) * 1000) / 1000;
-            }
-            temporaryMemory = []
-            memory = []
-            temporaryMemory.push(screenBottom.textContent);
-            screenTop.textContent = screenBottom.textContent;
+            equal()
         } else if (button.textContent === "C") {
             clearScreen();
-            memory = [];
-            temporaryMemory = [];
         } else if (checkOperator(screenBottom.textContent.slice(-1), button.textContent)) {
-            screenBottom.textContent = screenBottom.textContent.slice(0, -1) + button.textContent;
-            memory.pop();
-            memory.push(button.textContent);
+            changeLastOperator(button.textContent)
         } else {
             screenBottom.textContent += button.textContent
             if (memory.length == 0 && temporaryMemory.length == 0 && (button.textContent === "+" || button.textContent === "x" || button.textContent === "/")) {
-                
+                changeLastOperator(button.textContent)
             } else if (memory.length == 0 && temporaryMemory.length == 0 && button.textContent === "-") {
                 temporaryMemory.push(button.textContent)
-            } else if ((button.textContent === "+" || button.textContent === "-" || button.textContent === "x" || button.textContent === "/")) {
+            } else if (button.textContent === "+" || button.textContent === "-" || button.textContent === "x" || button.textContent === "/") {
                 if (temporaryMemory.length != 0){
                     number = parseFloat(temporaryMemory.join(""));
                     memory.push(number);
@@ -109,3 +146,5 @@ document.querySelectorAll("button").forEach((button) => {
         console.log("Memory: " + memory);
     });
 });
+
+keyboardListener()
